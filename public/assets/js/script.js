2e1c3ed4f932c5aa82f732faab4e1c452923c4ae
vitogallo <vito.gallo94@gmail.com>
2019-03-05 12:13:59 +0100

    Aggiunta ChatKey

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,8 +1,13 @@
 function createNewChat(name, user, description, image) {
-    firebase.database().ref('chats/').push({
+    var chatRef = firebase.database().ref('chats/')
+    var randomChatKey = chatRef.push().key
+    var prefix = "chat"
+    var newChatKey = prefix + randomChatKey
+    var newChatRef = firebase.database().ref('chats/' + newChatKey).set({
         name: name,
         user: user,
         description: description,
         image: image
     })
 }
+
