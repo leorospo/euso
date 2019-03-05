a6c1a545071afe97139240e511e3f915712597ea
Rogi Stenta <rogistenta@gmail.com>
2019-03-05 14:59:49 +0100

    test commit

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,27 +1,22 @@
-<<<<<<< HEAD
 var database = firebase.database();
-
-
 function writeMessage(userId, time, text, chatId) {
-var messagesRef = firebase.database().ref('chats/' + chatId + '/messages/');
-messagesRef.push({
-senderId:userId,
-time:time,
-text:text
-});
-}
-=======
-function createNewChat(name, user, description, image) {
-    var chatRef = firebase.database().ref('chats/')
-    var randomChatKey = chatRef.push().key
-    var prefix = "chat"
-    var newChatKey = prefix + randomChatKey
-    var newChatRef = firebase.database().ref('chats/' + newChatKey).set({
-        name: name,
-        user: user,
-        description: description,
-        image: image
+    var messagesRef = firebase.database().ref('chats/' + chatId + '/messages/');
+    var messageId = messagesRef.push().key
+    var prefix = 'msg'
+    var newMessageId = prefix + messageId
+
+    var newMessageRef = firebase.database().ref('chats/' + chatId + '/messages/'+ newMessageId)
+    newMessageRef.set({
+        senderId:userId,
+        time:time,
+        text:text
     })
+
 }
 
->>>>>>> 2e1c3ed4f932c5aa82f732faab4e1c452923c4ae
+
+
+var readMessage = firebase.database().ref('chats/' + chatId + '/messages/');
+readMessage.on('value', function(snapshot) {
+  updateStarCount(postElement, snapshot.val());
+});
