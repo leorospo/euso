4b3eef178419ded3376fa533df68f09d9dc93b13
vitogallo <vito.gallo94@gmail.com>
2019-03-05 14:59:35 +0100

    Prova

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,13 +1,18 @@
-function createNewChat(name, user, description, image) {
+function createNewChat(participants, title) {
+    var participants = [];
+    var title = "";
     var chatRef = firebase.database().ref('chats/')
     var randomChatKey = chatRef.push().key
     var prefix = "chat"
     var newChatKey = prefix + randomChatKey
-    var newChatRef = firebase.database().ref('chats/' + newChatKey).set({
-        name: name,
-        user: user,
-        description: description,
-        image: image
-    })
+    if (title != null) {
+        var newChatRef = firebase.database().ref('chats/' + newChatKey).set({
+            participants: participants,
+            title: title
+        })
+    } else console.error(err)
 }
 
+
+
+// controllo array, string, no title = no db
