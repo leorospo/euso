6e8e871dff1f088f2e57a97ba48b57dd2fd84614
vitogallo <vito.gallo94@gmail.com>
2019-03-06 12:42:31 +0100

    Aggiunta controlli e inizializzazione di firebase

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,13 +1,22 @@
+document.getElementsByClassName("button").addEventListener("click", createNewChat)
+
+
 function createNewChat(participants, title) {
-    var chatRef = firebase.database().ref('chats/')
-    var randomChatKey = chatRef.push().key
-    var prefix = "chat"
-    var newChatKey = prefix + randomChatKey
-    var newChatRef = firebase.database().ref('chats/' + newChatKey).set({
-        participants: participants,
-        title: title
-    })
+    if (typeof participants == "object") {
+        var chatRef = firebase.database().ref('chats/')
+        var randomChatKey = chatRef.push().key
+        var prefix = "chat"
+        var newChatKey = prefix + randomChatKey
+        var newChatRef = firebase.database().ref('chats/' + newChatKey)
+        if (typeof title == "string") {
+            newChatRef.set({
+                participants: participants,
+                title: title
+            })
+        } else {
+            newChatRef.set({
+                participants: participants,
+            })
+        }
+    }
 }
-
-
-
